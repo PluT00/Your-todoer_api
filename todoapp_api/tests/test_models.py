c1cde6c6320da7f95684ed4b0d1f78b97d7f4012
PluT00 <valeran2003@mail.ru>
2020-11-06 02:14:32 +0300

    Add unit tests for Task model

---
+++ todoapp_api/tests/test_models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from todoapp_api.models import Task
+
+
+class TaskModelTestCase(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="test", password="test")
+        self.task = Task.objects.create(
+            title="test_task", is_completed=False, user=self.user
+        )
+
+    def test_title_field(self):
+        max_length = self.task._meta.get_field('title').max_length
+        self.assertEqual(max_length, 1500)
+
+    def test_user_field(self):
+        related_model = self.task._meta.get_field('user').related_model
+        self.assertEqual(related_model, User)
+
+    def test__str__method(self):
+        self.assertEqual(
+            self.task.__str__(),
+            f'{self.task.id} | {self.task.title}'
+        )
