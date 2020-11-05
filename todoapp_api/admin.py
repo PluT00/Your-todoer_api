6eb71d06bf4dbd9e1bdd7e3152376f84e19960b4
PluT00 <valeran2003@mail.ru>
2020-11-06 01:52:42 +0300

    Add and register to admin Task model

--- todoapp_api/admin.py
+++ todoapp_api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from todoapp_api.models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    fields = [
+        'title',
+        'is_completed',
+        'user'
+    ]
+
+admin.site.register(Task, TaskAdmin)
--- todoapp_api/models.py
+++ todoapp_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=2000)
+    is_completed = models.BooleanField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.id} | {self.title}'
