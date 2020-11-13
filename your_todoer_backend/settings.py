b02e3428ff3e51b5081e4cf137217a5e994c3ede
PluT00 <valeran2003@mail.ru>
2020-11-13 18:51:54 +0300

    Clear defaults in db config. Use socket as default HOST

--- your_todoer_backend/settings.py
+++ your_todoer_backend/settings.py
@@ -80,11 +80,11 @@ WSGI_APPLICATION = 'your_todoer_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB', 'your_todoer'),
-        'USER': os.environ.get('POSTGRES_USER', 'root'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('POSTGRES_PORT', '5432')
+        'NAME': os.environ.get('POSTGRES_DB', ''),
+        'USER': os.environ.get('POSTGRES_USER', ''),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
+        'HOST': os.environ.get('POSTGRES_HOST', ''),
+        'PORT': os.environ.get('POSTGRES_PORT', '')
     }
 }
 
